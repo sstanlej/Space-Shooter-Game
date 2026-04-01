11dedab67ef32a40bd765f8ff3511dd1759944b7
sstanlej <sstanlejj@gmail.com>
2026-04-01 13:21:39 +0200

    Simplified stop_background script

--- stop_background.gd
+++ stop_background.gd
@@ -4,8 +4,4 @@ extends Sprite2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_background"):
-		if animation_player.active == false:
-			animation_player.active = true
-		else:
-			animation_player.active = false
-		
+		animation_player.active = !animation_player.active
