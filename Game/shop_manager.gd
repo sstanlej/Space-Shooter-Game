59c3af52ec0663775c5898ed1958e73edeaa52b0
sstanlej <sstanlejj@gmail.com>
2026-04-07 14:07:59 +0200

    Added show and hide animation for ShopManager

--- Game/shop_manager.gd
+++ Game/shop_manager.gd
@@ -2,12 +2,14 @@ class_name ShopManager extends Sprite2D
 
 @onready var game_manager: GameManager = $"../GameManager"
 @onready var shop_timer: Timer = $ShopTimer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	await get_tree().process_frame
 
 func show_shop() -> void:
 	shop_timer.start()
+	animation_player.play("show")
 	print("showing shop")
 
 func _process(_delta: float) -> void:
@@ -15,4 +17,5 @@ func _process(_delta: float) -> void:
 
 func _on_shop_timer_timeout() -> void:
 	print("shop timer finished")
+	animation_player.play("hide")
 	game_manager.start_next_wave()
