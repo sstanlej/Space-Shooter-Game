b6437101b02608168677b79da8b0ff6be4e5a9f4
sstanlej <sstanlejj@gmail.com>
2026-04-08 18:19:43 +0200

    Expanded shop logic, shop pannel slides in and out and allows to choose from 3 template upgrade options

--- Game/Scripts/shop_manager.gd
+++ Game/Scripts/shop_manager.gd
@@ -4,18 +4,49 @@ class_name ShopManager extends Sprite2D
 @onready var shop_timer: Timer = $ShopTimer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@onready var options = [$ShopOptions/AttackSpeedUpgrade, $ShopOptions/AttackDamageUpgrade, $ShopOptions/AttackDamageUpgrade2]
+@onready var selected_icon: Sprite2D = $ShopOptions/SelectedIcon
+
+var current_option: Sprite2D
+var previous_option: Sprite2D
+var next_option: Sprite2D
+var active: bool = false
+var selected: int = 0
+
 func _ready() -> void:
 	await get_tree().process_frame
+	previous_option = null
+	current_option = options[0]
+	next_option = options[1]
+	selected_icon.position = current_option.position
 
 func show_shop() -> void:
 	shop_timer.start()
 	animation_player.play("show")
+	active = true
 	print("showing shop")
 
 func _process(_delta: float) -> void:
-	pass
+	if not active:
+		return
+	if Input.is_action_just_pressed("right") and selected < options.size()-1:
+		selected += 1
+		previous_option = current_option
+		current_option = next_option
+		next_option = options[selected+1] if selected < options.size()-1 else null
+		selected_icon.position = current_option.position
+		print(selected)
+	if Input.is_action_just_pressed("left") and selected > 0:
+		selected -= 1
+		next_option = current_option
+		current_option = previous_option
+		previous_option = options[selected-1] if selected > 0 else null
+		selected_icon.position = current_option.position
+		print(selected)
+
 
 func _on_shop_timer_timeout() -> void:
 	print("shop timer finished")
 	animation_player.play("hide")
+	active = false
 	game_manager.start_next_wave()
