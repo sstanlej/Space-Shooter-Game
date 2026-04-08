81e6f11483ab19a01db7716ad9704003ba0942d2
sstanlej <sstanlejj@gmail.com>
2026-04-08 22:13:28 +0200

    Added attack_controler reference to PlayerUpgrade class

--- Upgrades/Scripts/player_upgrade.gd
+++ Upgrades/Scripts/player_upgrade.gd
@@ -1,6 +1,7 @@
 class_name PlayerUpgrade extends Sprite2D
 
 @onready var player: Player = $"../../../Player"
+@onready var player_attack_controler: AttackControler = $"../../../Player/AttackControler"
 @export var level: float = 1
 @export var description: String = "I am an upgrade"
 
