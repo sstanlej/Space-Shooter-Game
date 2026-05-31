7f3acd9070675512b837b2e4e1510ea6cda4e132
sstanlej <sstanlejj@gmail.com>
2026-05-31 17:47:06 +0200

    Started working on locations logic

---
+++ Game/locations/Scripts/LocationData.gd
@@ -0,0 +1,9 @@
+class_name LocationData extends Resource
+
+@export var location_id: int
+@export var location_name: String
+
+@export var background_texture: Texture2D
+@export var middle_texture: Texture2D
+@export var front_texture: Texture2D
+
