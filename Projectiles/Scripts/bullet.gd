a928e0d1b5d6c9d2c22aabbc8ce42edf6303ef69
sstanlej <sstanlejj@gmail.com>
2026-07-30 15:58:15 +0200

    Renamed Bullet folder to Projectiles

--- Projectiles/Scripts/bullet.gd
+++ Projectiles/Scripts/bullet.gd
@@ -2,12 +2,12 @@ class_name Bullet extends Area2D
 
 @export var move_speed : float = 200
 @export var attack_damage : float = 1
-static var default_sprite = preload("res://Bullet/Sprites/bullet.png")
-static var boosted_sprite = preload("res://Bullet/Sprites/boosted_bullet.png")
+static var default_sprite = preload("res://Projectiles/Sprites/bullet.png")
+static var boosted_sprite = preload("res://Projectiles/Sprites/boosted_bullet.png")
 var direction = Vector2.RIGHT
 
 static func spawn_bullet(dmg: float, speed: float, boosted: bool) -> Bullet:
-	var my_scene: PackedScene = load("res://Bullet/bullet.tscn")
+	var my_scene: PackedScene = load("res://Projectiles/bullet.tscn")
 	var new_bullet: Bullet = my_scene.instantiate()
 	new_bullet.set_damage(dmg)
 	new_bullet.set_speed(speed)
