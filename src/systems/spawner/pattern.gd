0ea53e4e3e8bb1a795bccd3c80d01c1fff046a39
stachu jones <sstanlejj@gmail.com>
2026-08-29 10:25:46 +0200

    Temporarily commented out old code to remove errors

--- src/systems/spawner/pattern.gd
+++ src/systems/spawner/pattern.gd
@@ -6,80 +6,80 @@ var size: int = 0
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
-func generate_random_base(pattern_size: int, start_x: int, min_x_gap: int, max_x_gap: int) -> void:
-	var x_values: Array = []
-	for i in range(pattern_size):
-		x_values.append(rng.randi_range(min_x_gap, max_x_gap))
-	x_values[0] = x_values[0] / 2 + start_x
-	for i in range(1, pattern_size):
-		x_values[i] += x_values[i-1]
-	for i in range(pattern_size):
-		var y: int = rng.randi_range(Spawner.min_y, Spawner.max_y)
-		var slot = PatternSlot.new(Vector2(x_values[i], y))
-		slots.append(slot)
-	size = pattern_size
+# func generate_random_base(pattern_size: int, start_x: int, min_x_gap: int, max_x_gap: int) -> void:
+# 	var x_values: Array = []
+# 	for i in range(pattern_size):
+# 		x_values.append(rng.randi_range(min_x_gap, max_x_gap))
+# 	x_values[0] = x_values[0] / 2 + start_x
+# 	for i in range(1, pattern_size):
+# 		x_values[i] += x_values[i-1]
+# 	for i in range(pattern_size):
+# 		var y: int = rng.randi_range(Spawner.min_y, Spawner.max_y)
+# 		var slot = PatternSlot.new(Vector2(x_values[i], y))
+# 		slots.append(slot)
+# 	size = pattern_size
 
-func make_vertical_cluster(slot_index: int, cluster_size: int, gap_y: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_vertical_cluster(cluster_size, gap_y)
-	size = get_pattern().size()
+# func make_vertical_cluster(slot_index: int, cluster_size: int, gap_y: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_vertical_cluster(cluster_size, gap_y)
+# 	size = get_pattern().size()
 
-func make_horizontal_cluster(slot_index: int, cluster_size: int, gap_x: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_horizontal_cluster(cluster_size, gap_x, is_position_safe)
-	size = get_pattern().size()
+# func make_horizontal_cluster(slot_index: int, cluster_size: int, gap_x: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_horizontal_cluster(cluster_size, gap_x, is_position_safe)
+# 	size = get_pattern().size()
 
-func make_key_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_key_cluster(cluster_size, gap_x, gap_y, is_position_safe)
-	size = get_pattern().size()
+# func make_key_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_key_cluster(cluster_size, gap_x, gap_y, is_position_safe)
+# 	size = get_pattern().size()
 
-func make_block_cluster(slot_index: int, cluster_size_x: int, cluster_size_y: int, gap_x: int, gap_y: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_block_cluster(cluster_size_x, cluster_size_y, gap_x, gap_y, is_position_safe)
-	size = get_pattern().size()
+# func make_block_cluster(slot_index: int, cluster_size_x: int, cluster_size_y: int, gap_x: int, gap_y: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_block_cluster(cluster_size_x, cluster_size_y, gap_x, gap_y, is_position_safe)
+# 	size = get_pattern().size()
 
-func make_rising_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_rising_cluster(cluster_size, gap_x, gap_y, is_position_safe)
-	size = get_pattern().size()
+# func make_rising_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_rising_cluster(cluster_size, gap_x, gap_y, is_position_safe)
+# 	size = get_pattern().size()
 
-func make_falling_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
-	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
-		slots[slot_index].make_falling_cluster(cluster_size, gap_x, gap_y, is_position_safe)
-	size = get_pattern().size()
+# func make_falling_cluster(slot_index: int, cluster_size: int, gap_x: int, gap_y: int) -> void:
+# 	if slot_index >= 0 and slot_index < slots.size() and slots[slot_index].type == "single":
+# 		slots[slot_index].make_falling_cluster(cluster_size, gap_x, gap_y, is_position_safe)
+# 	size = get_pattern().size()
 
-func make_cluster(slot_index: int, cluster_type: String, params: Dictionary) -> void:
-	if slot_index < 0 or slot_index >= slots.size() or slots[slot_index].type != "single":
-		return
-	var slot = slots[slot_index]
-	match cluster_type:
-		"vertical":
-			slot.make_vertical_cluster(params.get("size", 3), params.get("gap_y", 20))
-		"horizontal":
-			slot.make_horizontal_cluster(params.get("size", 3), params.get("gap_x", 20), is_position_safe)
-		"block":
-			slot.make_block_cluster(params.get("size_x", 2), params.get("size_y", 2), params.get("gap_x", 20), params.get("gap_y", 20), is_position_safe)
-		"rising":
-			slot.make_rising_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
-		"falling":
-			slot.make_falling_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
-		"key":
-			slot.make_key_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
-	size = get_pattern().size()
+# func make_cluster(slot_index: int, cluster_type: String, params: Dictionary) -> void:
+# 	if slot_index < 0 or slot_index >= slots.size() or slots[slot_index].type != "single":
+# 		return
+# 	var slot = slots[slot_index]
+# 	match cluster_type:
+# 		"vertical":
+# 			slot.make_vertical_cluster(params.get("size", 3), params.get("gap_y", 20))
+# 		"horizontal":
+# 			slot.make_horizontal_cluster(params.get("size", 3), params.get("gap_x", 20), is_position_safe)
+# 		"block":
+# 			slot.make_block_cluster(params.get("size_x", 2), params.get("size_y", 2), params.get("gap_x", 20), params.get("gap_y", 20), is_position_safe)
+# 		"rising":
+# 			slot.make_rising_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
+# 		"falling":
+# 			slot.make_falling_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
+# 		"key":
+# 			slot.make_key_cluster(params.get("size", 3), params.get("gap_x", 20), params.get("gap_y", 10), is_position_safe)
+# 	size = get_pattern().size()
 
-func is_position_safe(pos: Vector2) -> bool:
-	for slot in slots:
-		for point in slot.get_points():
-			if pos.distance_squared_to(point) < min_gap * min_gap:
-				return false
-	return true
+# func is_position_safe(pos: Vector2) -> bool:
+# 	for slot in slots:
+# 		for point in slot.get_points():
+# 			if pos.distance_squared_to(point) < min_gap * min_gap:
+# 				return false
+# 	return true
 
-func get_pattern() -> Array:
-	var points: Array = []
-	for slot in slots:
-		points += slot.get_points()
-	return points
+# func get_pattern() -> Array:
+# 	var points: Array = []
+# 	for slot in slots:
+# 		points += slot.get_points()
+# 	return points
 
-func get_size() -> int:
-	return size
+# func get_size() -> int:
+# 	return size
--- src/systems/spawner/pattern_slot.gd
+++ src/systems/spawner/pattern_slot.gd
@@ -4,91 +4,91 @@ var base_position: Vector2
 var points: Array[Vector2] = []
 var type: String # "single", "vertical_cluster", "horizontal_cluster"
 
-func _init(pos: Vector2) -> void:
-    base_position = pos
-    points.append(base_position)
-    type = "single"
+# func _init(pos: Vector2) -> void:
+#     base_position = pos
+#     points.append(base_position)
+#     type = "single"
 
-func make_vertical_cluster(cluster_size: int, gap_y: int) -> void:
-    type = "vertical_cluster"
-    points = []
-    for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
-        var new_point = Vector2(base_position.x, base_position.y + i * gap_y)
-        if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
-            print("Cluster point %s crossed limit" % new_point)
-            continue
-        points.append(new_point)
+# func make_vertical_cluster(cluster_size: int, gap_y: int) -> void:
+#     type = "vertical_cluster"
+#     points = []
+#     for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
+#         var new_point = Vector2(base_position.x, base_position.y + i * gap_y)
+#         if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
+#             print("Cluster point %s crossed limit" % new_point)
+#             continue
+#         points.append(new_point)
 
-func make_horizontal_cluster(cluster_size: int, gap_x: int, validator: Callable) -> void:
-    type = "horizontal_cluster"
-    points = []
-    for i in range(cluster_size):
-        var new_point = Vector2(base_position.x + i * gap_x, base_position.y)
-        if validator.call(new_point):
-            points.append(new_point)
-        else:
-            print("Cluster point %s is too close to another point, skipping" % new_point)
+# func make_horizontal_cluster(cluster_size: int, gap_x: int, validator: Callable) -> void:
+#     type = "horizontal_cluster"
+#     points = []
+#     for i in range(cluster_size):
+#         var new_point = Vector2(base_position.x + i * gap_x, base_position.y)
+#         if validator.call(new_point):
+#             points.append(new_point)
+#         else:
+#             print("Cluster point %s is too close to another point, skipping" % new_point)
 
-func make_key_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
-    type = "key_cluster"
-    points = [Vector2(base_position.x, base_position.y)]
-    for i in range(1, cluster_size / 2 + 1):
-        var new_point1 = Vector2(base_position.x + i * gap_x, base_position.y + i * gap_y)
-        var new_point2 = Vector2(base_position.x + i * gap_x, base_position.y - i * gap_y)
-        if new_point1.y > Spawner.max_y or new_point1.y < Spawner.min_y:
-            print("Cluster point %s crossed limit" % new_point1)
-            continue
-        if new_point2.y > Spawner.max_y or new_point2.y < Spawner.min_y:
-            print("Cluster point %s crossed limit" % new_point2)
-            continue
-        if validator.call(new_point1):
-            points.append(new_point1)
-        else:
-            print("Cluster point %s is too close to another point, skipping" % new_point1)
-        if validator.call(new_point2):
-            points.append(new_point2)
-        else:
-            print("Cluster point %s is too close to another point, skipping" % new_point2)
+# func make_key_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
+#     type = "key_cluster"
+#     points = [Vector2(base_position.x, base_position.y)]
+#     for i in range(1, cluster_size / 2 + 1):
+#         var new_point1 = Vector2(base_position.x + i * gap_x, base_position.y + i * gap_y)
+#         var new_point2 = Vector2(base_position.x + i * gap_x, base_position.y - i * gap_y)
+#         if new_point1.y > Spawner.max_y or new_point1.y < Spawner.min_y:
+#             print("Cluster point %s crossed limit" % new_point1)
+#             continue
+#         if new_point2.y > Spawner.max_y or new_point2.y < Spawner.min_y:
+#             print("Cluster point %s crossed limit" % new_point2)
+#             continue
+#         if validator.call(new_point1):
+#             points.append(new_point1)
+#         else:
+#             print("Cluster point %s is too close to another point, skipping" % new_point1)
+#         if validator.call(new_point2):
+#             points.append(new_point2)
+#         else:
+#             print("Cluster point %s is too close to another point, skipping" % new_point2)
 
-func make_block_cluster(cluster_size_x: int, cluster_size_y: int, gap_x: int, gap_y: int, validator: Callable) -> void:
-    type = "block_cluster"
-    points = []
-    for i in range(cluster_size_x):
-        for j in range(cluster_size_y):
-            var new_point = Vector2(base_position.x + i * gap_x, base_position.y - j * gap_y)
-            if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
-                print("Cluster point %s crossed limit" % new_point)
-                continue
-            if validator.call(new_point):
-                points.append(new_point)
-            else:
-                print("Cluster point %s is too close to another point, skipping" % new_point)
+# func make_block_cluster(cluster_size_x: int, cluster_size_y: int, gap_x: int, gap_y: int, validator: Callable) -> void:
+#     type = "block_cluster"
+#     points = []
+#     for i in range(cluster_size_x):
+#         for j in range(cluster_size_y):
+#             var new_point = Vector2(base_position.x + i * gap_x, base_position.y - j * gap_y)
+#             if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
+#                 print("Cluster point %s crossed limit" % new_point)
+#                 continue
+#             if validator.call(new_point):
+#                 points.append(new_point)
+#             else:
+#                 print("Cluster point %s is too close to another point, skipping" % new_point)
 
-func make_rising_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
-    type = "rising_cluster"
-    points = []
-    for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
-        var new_point = Vector2(base_position.x + i * gap_x, base_position.y - i * gap_y)
-        if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
-            print("Cluster point %s crossed limit" % new_point)
-            continue
-        if validator.call(new_point):
-            points.append(new_point)
-        else:
-            print("Cluster point %s is too close to another point, skipping" % new_point)
+# func make_rising_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
+#     type = "rising_cluster"
+#     points = []
+#     for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
+#         var new_point = Vector2(base_position.x + i * gap_x, base_position.y - i * gap_y)
+#         if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
+#             print("Cluster point %s crossed limit" % new_point)
+#             continue
+#         if validator.call(new_point):
+#             points.append(new_point)
+#         else:
+#             print("Cluster point %s is too close to another point, skipping" % new_point)
 
-func make_falling_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
-    type = "falling_cluster"
-    points = []
-    for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
-        var new_point = Vector2(base_position.x + i * gap_x, base_position.y + i * gap_y)
-        if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
-            print("Cluster point %s crossed limit" % new_point)
-            continue
-        if validator.call(new_point):
-            points.append(new_point)
-        else:
-            print("Cluster point %s is too close to another point, skipping" % new_point)
+# func make_falling_cluster(cluster_size: int, gap_x: int, gap_y: int, validator: Callable) -> void:
+#     type = "falling_cluster"
+#     points = []
+#     for i in range(-cluster_size/2, cluster_size/2 + cluster_size % 2):
+#         var new_point = Vector2(base_position.x + i * gap_x, base_position.y + i * gap_y)
+#         if new_point.y > Spawner.max_y or new_point.y < Spawner.min_y:
+#             print("Cluster point %s crossed limit" % new_point)
+#             continue
+#         if validator.call(new_point):
+#             points.append(new_point)
+#         else:
+#             print("Cluster point %s is too close to another point, skipping" % new_point)
 
-func get_points() -> Array[Vector2]:
-    return points
+# func get_points() -> Array[Vector2]:
+#     return points
