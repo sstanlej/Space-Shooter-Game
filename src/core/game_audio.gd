6f863f1dfa1336b2655404746bd57c484fbb69fb
stachu jones <sstanlejj@gmail.com>
2026-08-29 19:03:41 +0200

    Added template background music loop

--- src/core/game_audio.gd
+++ src/core/game_audio.gd
@@ -8,6 +8,32 @@ class_name GameAudio extends Node2D
 @onready var select_sound : AudioStreamPlayer2D = $SelectSound
 @onready var levelup_sound : AudioStreamPlayer2D = $LevelUpSound
 
+@export var background_music: AudioStream
+var music_player: AudioStreamPlayer
+
+func _ready() -> void:
+	setup_music_player()
+
+func setup_music_player() -> void:
+	music_player = AudioStreamPlayer.new()
+	music_player.name = "BackgroundMusicPlayer"
+	music_player.process_mode = Node.PROCESS_MODE_ALWAYS 
+	add_child(music_player)
+
+	if background_music:
+		play_music(background_music)
+
+func play_music(stream: AudioStream, volume_db: float = -15.0) -> void:
+	if not stream or not music_player:
+		return
+	music_player.stream = stream
+	music_player.volume_db = volume_db
+	music_player.play()
+
+func stop_music() -> void:
+	if music_player:
+		music_player.stop()
+
 func play_crash() -> void:
 	crash_sound.play()
 
